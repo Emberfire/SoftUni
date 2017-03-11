7364de5aecbd299b7459833ce19c2fe7d8975daa
Emberfire <petar6464@gmail.com>
2017-03-11 09:58:57 +0200

    Testing intellij committing

--- Java OOP Basics/Labs/Polymorphism/src/Shapes/Main.java
+++ Java OOP Basics/Labs/Polymorphism/src/Shapes/Main.java
@@ -8,33 +8,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(console.readLine());
-        for (int i = 0; i < n ; i++)
-        {
-            for (int j = 0; j < n - i - 1; j++)
-            {
-                System.out.print(" ");
-
-            }
-            for (int j = 0; j < i + 1 ; j++)
-            {
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-        for (int i = 0; i < n - 1; i++)
-        {
-            for (int j = 0; j < i + 1; j++)
-            {
-                System.out.print(" ");
-                
-            }
-            for (int j = 0; j < n- i - 1; j++)
-            {
-                System.out.print( "+ ");
-                
-            }
-            System.out.println();
-        }
+        
     }
 }
