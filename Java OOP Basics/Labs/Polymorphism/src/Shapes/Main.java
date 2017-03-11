0e889bd5974b5db4ab38b7d73651cc87456224ab
Emberfire <petar6464@gmail.com>
2017-03-11 10:27:44 +0200

    Added Polymorphism lab.

--- Java OOP Basics/Labs/Polymorphism/src/Shapes/Main.java
+++ Java OOP Basics/Labs/Polymorphism/src/Shapes/Main.java
@@ -1,13 +1,8 @@
 package Shapes;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-
-        
     }
 }
